610b650a1926c8ed5736cd4239433cb60679c6bd
United137 <united137@yandex.ry>
2022-11-30 14:16:42 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,145 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+testi=[]
+
+def get_first_news():
+    
+    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
+
+    url='https://www.championat.com/football/'
+    r=requests.get(url=url, headers=headers)
+
+    soup =BeautifulSoup(r.text, 'lxml')
+
+    article_cards=soup.find_all('div', class_='article-preview')
+    article_cards_top=soup.find_all('div', class_='top-article')
+    article_cards_top_small=soup.find_all('div', class_='top-article _small')
+
+    dict_news={}
+    
+    for article in article_cards_top:
+        article_title=article.find('a', class_='top-article__title').text.strip()
+        article_url=article.find('a').get('href')
+        # print(article_title, article_url)
+
+        article_id=''.join(article_url.split('/')[-1]).split('-')[1]
+
+        dict_news[article_id]={
+            'article_title':article_title,
+            'article_url':article_url
+        }
+
+    for article in article_cards_top_small:
+        article_title=article.find('a', class_='top-article__title').text.strip()
+        article_url=article.find('a').get('href')
+        # print(article_title, article_url)
+
+        article_id=''.join(article_url.split('/')[-1]).split('-')[1]
+
+        dict_news[article_id]={
+            'article_title':article_title,
+            'article_url':article_url
+        }
+        
+
+    for article in article_cards:
+        article_title=article.find('a', class_='article-preview__title').text.strip()
+        article_url=article.find('a').get('href')
+        
+
+        article_id=''.join(article_url.split('/')[-1]).split('-')[1]
+
+        dict_news[article_id]={
+            'article_title':article_title,
+            'article_url':article_url
+        }
+        
+    
+    with open('news_dic.json', 'w', encoding='utf-8') as file:
+        json.dump(dict_news, file, indent=4, ensure_ascii=False)
+
+    print(f'Finish')
+
+def check_news_update():
+    fresh_news={}
+    with open('news_dic.json', encoding='utf-8') as file:
+        dict_news=json.load(file)
+    
+    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
+
+    url='https://www.championat.com/football/'
+    r=requests.get(url=url, headers=headers)
+
+    soup =BeautifulSoup(r.text, 'lxml')
+
+    article_cards=soup.find_all('div', class_='article-preview')
+    article_cards_top=soup.find_all('div', class_='top-article')
+    article_cards_top_small=soup.find_all('div', class_='top-article _small')
+
+    for article in article_cards:
+        article_url=article.find('a').get('href')
+        article_id=''.join(article_url.split('/')[-1]).split('-')[1]
+
+        if article_id in dict_news:
+            continue
+        else:
+            article_title=article.find('a', class_='article-preview__title').text.strip()
+            article_url=article.find('a').get('href')
+            dict_news[article_id]={
+            'article_title':article_title,
+            'article_url':article_url
+        }
+            fresh_news[article_id]={
+            'article_title':article_title,
+            'article_url':article_url
+        }
+    for article in article_cards_top:
+        article_url=article.find('a').get('href')
+        article_id=''.join(article_url.split('/')[-1]).split('-')[1]
+
+        if article_id in dict_news:
+            continue
+        else:
+            article_title=article.find('a', class_='top-article__title').text.strip() 
+            article_url=article.find('a').get('href')
+            dict_news[article_id]={
+            'article_title':article_title,
+            'article_url':article_url
+        }
+            fresh_news[article_id]={
+            'article_title':article_title,
+            'article_url':article_url
+        }
+    for article in article_cards_top_small: 
+        article_url=article.find('a').get('href')
+        article_id=''.join(article_url.split('/')[-1]).split('-')[1]
+
+        if article_id in dict_news:
+            continue
+        else:
+            article_title=article.find('a', class_='article-preview__title').text.strip()
+            article_url=article.find('a').get('href')
+            dict_news[article_id]={
+            'article_title':article_title,
+            'article_url':article_url
+        }
+            fresh_news[article_id]={
+            'article_title':article_title,
+            'article_url':article_url
+        }
+
+    with open('news_dic.json', 'w', encoding='utf-8') as file:
+        json.dump(dict_news, file, indent=4, ensure_ascii=False)
+    
+    return fresh_news
+
+def main():
+    # get_first_news()
+    print(check_news_update())
+
+
+if __name__=='__main__': main()
+    
---
+++ tg_bot.py
@@ -0,0 +1,74 @@
+import asyncio
+import json
+from aiogram import Bot, Dispatcher, executor, types
+from config import token, user_id
+from aiogram.utils.markdown import hbold, hunderline, hcode, hlink
+from main import check_news_update
+from aiogram.dispatcher.filters import Text
+
+bot =Bot(token=token, parse_mode=types.ParseMode.HTML)
+dp=Dispatcher(bot)
+
+@dp.message_handler(commands="Go")
+async def start(message: types.Message):
+    start_button=['All news', 'Last 5 news', 'Fresh news']
+    keyboard=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*start_button)
+
+    await message.answer('Lenta news', reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals='All news'))
+async def get_all_news(message: types.Message):
+    with open('news_dic.json','r', encoding='utf-8') as file:
+        news_dict=json.load(file)
+    
+    for k, v in sorted(news_dict.items()):
+        news=f"{hunderline(v['article_title'])}\n {v['article_url']}"
+    
+        await message.answer(news)
+
+@dp.message_handler(Text(equals='Last 5 news'))
+async def get_last_five_news(message: types.Message):
+    with open('news_dic.json','r', encoding='utf-8') as file:
+        news_dict=json.load(file)
+    
+    for k, v in sorted(news_dict.items())[-5:]:
+        news=f"{hunderline(v['article_title'])}\n {v['article_url']}"
+    
+        await message.answer(news)
+
+@dp.message_handler(Text(equals='Fresh news'))
+async def get_fresh_news(message: types.Message):
+    fresh_news=check_news_update()
+
+    if len(fresh_news)>=1:
+        for k, v in sorted(fresh_news.items())[-5:]:
+            news=f"{hunderline(v['article_title'])}\n {v['article_url']}"
+    
+            await message.answer(news)
+    
+    else: 
+        await message.answer('dont have any news')
+
+async def news_every_minute():
+    while True:
+        fresh_news=check_news_update()
+
+        if len(fresh_news)>=1:
+            for k, v in sorted(fresh_news.items()):
+                news=f"{hunderline(v['article_title'])}\n {v['article_url']}"
+
+                await bot.send_message(user_id, news, disable_notification=True)
+                '''not noyze'''
+        
+        # else: 
+        #     """for informations"""
+        #     await bot.send_message(user_id, "not new news",disable_notification=True) 
+
+        await asyncio.sleep(20)   
+
+if __name__=='__main__': 
+    loop=asyncio.get_event_loop()
+    loop.create_task(news_every_minute())
+    executor.start_polling(dp)
